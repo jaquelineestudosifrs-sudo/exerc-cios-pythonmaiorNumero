e046595362eee956fb2330a4a70ad02c265d9a3b
jaquelineestudosifrs-sudo <jaquelineestudosifrs@gmail.com>
2025-08-29 23:57:46 -0300

    Add files via upload

---
+++ maiorNumero_2.py
@@ -0,0 +1,8 @@
+numero1 = int(input("digite o primeiro numero:"))
+numero2 = int(input("digite o segundo numero:"))
+if numero1 > numero2:
+    print(f"o maior numero é {numero1}")
+else:
+    print(f"o maior numero é {numero2}")
+          
+
